76a9f10b8d4e4aa3090004b6cb7aa06529019421
Pedro Melo <pedro.melo@gerafuturo.com.br>
2017-08-24 17:24:01 -0300

    convert controller to web api

--- Zx/Controllers/PdvController.cs
+++ Zx/Controllers/PdvController.cs
@@ -1,68 +1,60 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
-using System.Web.Mvc;
-using Zx.Models;
+using System.Web.Http;
 
 namespace Zx.Controllers
 {
-    public class PdvController : Controller
-    {        
+    public class PdvController : ApiController
+    {
         private JArray GetPdvs()
         {
             const string pdvsCacheKey = "pdvs";
             var cached = HttpRuntime.Cache.Get(pdvsCacheKey);
             var result = new JArray();
-            if(cached != null)
+            if (cached != null)
             {
                 return (JArray)cached;
             }
             else
             {
-                var rawObj = JObject.Parse(System.IO.File.ReadAllText(Server.MapPath(@"~\App_Data\pdvs.json")));
+                var rawObj = JObject.Parse(System.IO.File.ReadAllText(System.Web.Hosting.HostingEnvironment.MapPath(@"~\App_Data\pdvs.json")));
                 result = (JArray)rawObj["pdvs"];
                 HttpRuntime.Cache.Insert(pdvsCacheKey, result);
                 return result;
-            } 
+            }
         }
 
-        public ActionResult Get(int id)
+        // GET: api/Pdv
+        public JArray Get()
         {
-            var pdvs = GetPdvs();
-            var pdv = (JObject)pdvs.FirstOrDefault(x => (int)x["id"] == id);
-            return Content(pdv.ToString(), "application/json");
+            return GetPdvs();
         }
 
-        [HttpPost]
-        public ActionResult Create()
+        // GET: api/Pdv/5
+        public JObject Get(int id)
         {
-            var errors = new List<string>();
-
-            //Read json from request
-            var req = Request.InputStream;
-            req.Seek(0, SeekOrigin.Begin);
-            var jsonRequest = new StreamReader(req).ReadToEnd();
-            var pdv = JObject.Parse(jsonRequest);
-
-            //Fetch full db for validating unique document
             var pdvs = GetPdvs();
+            return (JObject)pdvs.FirstOrDefault(x => (int)x["id"] == id);
+        }
 
-            //Validate mandatory fields
-            if ((int?)pdv["id"] < 1) errors.Add("Invalid Id");
-            if (string.IsNullOrEmpty((string)pdv["tradingName"])) errors.Add("Invalid Trading Name");
-            if (string.IsNullOrEmpty((string)pdv["ownerName"])) errors.Add("Invalid Owner Name");
-            if (string.IsNullOrEmpty((string)pdv["document"])) errors.Add("Invalid Document");
-            //coverageArea (...)
-            //address (...)
-            if ((int?)pdv["deliveryCapacity"] == null) errors.Add("Invalid Capacity");
+        // POST: api/Pdv
+        public void Post([FromBody]string value)
+        {
+        }
 
-            //Validate existing CNPJ
-            if (pdvs.Any(x => (string)x["document"] == (string)pdv["document"])) errors.Add("Document must be unique within database");
+        // PUT: api/Pdv/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
 
-            return Content(pdv.ToString(), "application/json");
+        // DELETE: api/Pdv/5
+        public void Delete(int id)
+        {
         }
     }
 }
