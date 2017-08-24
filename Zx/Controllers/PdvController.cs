3262766fb6d922139834b79adc3540a0089aedff
Pedro Melo <pedro.melo@gerafuturo.com.br>
2017-08-24 16:24:42 -0300

    code cleaning; add pdv controller; read pdvs from file

---
+++ Zx/Controllers/PdvController.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zx.Controllers
+{
+    public class PdvController : Controller
+    {        
+        private JArray GetPdvs()
+        {
+            var rawObj = JObject.Parse(System.IO.File.ReadAllText(Server.MapPath(@"~\App_Data\pdvs.json")));
+            return (JArray)rawObj["pdvs"]; 
+        }
+
+        public ActionResult Get(int id)
+        {
+            var pdvs = GetPdvs();
+
+            return Content("{}", "application/json");
+        }
+
+
+    }
+}
