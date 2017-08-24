202f0bd1df13871db8c4bdda3141aaaec6f93798
Pedro Melo <pedro.melo@gerafuturo.com.br>
2017-08-24 16:44:12 -0300

    started implementing pdv creation

--- Zx/Controllers/PdvController.cs
+++ Zx/Controllers/PdvController.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zx.Models;
 
 namespace Zx.Controllers
 {
@@ -22,6 +24,33 @@ namespace Zx.Controllers
             return Content(pdv.ToString(), "application/json");
         }
 
+        [HttpPost]
+        public ActionResult Create()
+        {
+            var errors = new List<string>();
 
+            //Read json from request
+            var req = Request.InputStream;
+            req.Seek(0, SeekOrigin.Begin);
+            var jsonRequest = new StreamReader(req).ReadToEnd();
+            var pdv = JObject.Parse(jsonRequest);
+
+            //Fetch full db for validating unique document
+            var pdvs = GetPdvs();
+
+            //Validate mandatory fields
+            if ((int?)pdv["id"] < 1) errors.Add("Invalid Id");
+            if (string.IsNullOrEmpty((string)pdv["tradingName"])) errors.Add("Invalid Trading Name");
+            if (string.IsNullOrEmpty((string)pdv["ownerName"])) errors.Add("Invalid Owner Name");
+            if (string.IsNullOrEmpty((string)pdv["document"])) errors.Add("Invalid Document");
+            //coverageArea (...)
+            //address (...)
+            if ((int?)pdv["deliveryCapacity"] == null) errors.Add("Invalid Capacity");
+
+            //Validate existing CNPJ
+            if (pdvs.Any(x => (string)x["document"] == (string)pdv["document"])) errors.Add("Document must be unique within database");
+
+            return Content(pdv.ToString(), "application/json");
+        }
     }
 }
---
+++ Zx/Models/CommandResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zx.Models
+{
+    public class CommandResponse
+    {
+        public bool success { get; private set; }
+        public IEnumerable<string> errors { get; private set; }
+        public object data { get; private set; }
+
+        public CommandResponse(bool success, object data)
+        {
+            this.success = success;
+            errors = new List<string>();
+            this.data = data;
+        }
+
+        public CommandResponse(bool success, IEnumerable<string> errors, object data)
+        {
+            this.success = success;
+            this.errors = errors;
+            this.data = data;
+        }
+    }
+}
