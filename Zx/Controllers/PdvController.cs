d4e26c46b9b62bc8c723b797c22c04e1c0a8d7bc
Pedro Melo <pscelzamelo@gmail.com>
2017-08-25 08:30:29 -0300

    create pdv;

--- Zx/Controllers/PdvController.cs
+++ Zx/Controllers/PdvController.cs
@@ -14,10 +14,11 @@ namespace Zx.Controllers
 {
     public class PdvController : ApiController
     {
+        const string _pdvsCacheKey = "pdvs";
+
         private JArray GetPdvs()
         {
-            const string pdvsCacheKey = "pdvs";
-            var cached = HttpRuntime.Cache.Get(pdvsCacheKey);
+            var cached = HttpRuntime.Cache.Get(_pdvsCacheKey);
             var result = new JArray();
             if (cached != null)
             {
@@ -27,7 +28,7 @@ namespace Zx.Controllers
             {
                 var rawObj = JObject.Parse(System.IO.File.ReadAllText(System.Web.Hosting.HostingEnvironment.MapPath(@"~\App_Data\pdvs.json")));
                 result = (JArray)rawObj["pdvs"];
-                HttpRuntime.Cache.Insert(pdvsCacheKey, result);
+                HttpRuntime.Cache.Insert(_pdvsCacheKey, result);
                 return result;
             }
         }
@@ -49,10 +50,7 @@ namespace Zx.Controllers
         public CommandResponse Post(JObject pdv)
         {
             var errors = new List<string>();
-
-            //Fetch full db for validating unique document
-            var pdvs = GetPdvs();
-
+            
             //Validate mandatory fields
             if ((int?)pdv["id"] == null || (int?)pdv["id"] < 1) errors.Add("Invalid Id");
             if (string.IsNullOrEmpty((string)pdv["tradingName"])) errors.Add("Invalid Trading Name");
@@ -60,19 +58,23 @@ namespace Zx.Controllers
             if (string.IsNullOrEmpty((string)pdv["document"])) errors.Add("Invalid Document");
             if ((int?)pdv["deliveryCapacity"] == null) errors.Add("Invalid Capacity");
             var coverageArea = JsonConvert.DeserializeObject<MultiPolygon>(pdv["coverageArea"]?.ToString());
-            if(coverageArea == null) errors.Add("Invalid Coverage Area");
+            if (coverageArea == null) errors.Add("Invalid Coverage Area");
             var address = JsonConvert.DeserializeObject<Point>(pdv["address"]?.ToString());
             if (address == null) errors.Add("Invalid Address");
 
             //Validate existing CNPJ
+            var pdvs = GetPdvs();
             if (pdvs.Any(x => (string)x["document"] == (string)pdv["document"])) errors.Add("Document must be unique within database");
 
+            //Return errors or persist
             if (errors.Count > 0)
             {
                 return new CommandResponse(false, errors, pdv);
             }
             else
             {
+                pdvs.Add(pdv);
+                HttpRuntime.Cache.Insert(_pdvsCacheKey, pdvs);
                 return new CommandResponse(true, pdv);
             }
         }
